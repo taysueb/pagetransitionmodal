75482fa470fee79f631c516531647677135fb07a
Chaicharn Vijarnsatit <TayS@Chaicharns-MacBook-Pro.local>
2017-09-14 10:23:24 +0700

    Initial Commit

---
+++ pagetransitionmodal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pagetransitionmodal
+//
+//  Created by Chaicharn Vijarnsatit on 9/14/2560 BE.
+//  Copyright © 2560 Chaicharn Vijarnsatit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
